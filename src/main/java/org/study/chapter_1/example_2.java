f211bc50b7899c010a2643212537e8c9b07d85fe
Nazar-Mik <nazarmik98@gmail.com>
2023-09-04 20:23:46 +0300

    added example if

---
+++ src/main/java/org/study/chapter_1/example_2.java
@@ -0,0 +1,15 @@
+package org.study.chapter_1;
+
+public class example_2 {
+    public static void main(String[] args) {
+
+        int num;
+        num = 100;
+
+        System.out.println("Значення num = " + num);
+
+        num = num *2;
+        System.out.println("Значення num * 2 = " + num);
+
+    }
+}
