9811bfaba550e62e9d89cc1cdc75dd638921f533
Nazar-Mik <nazarmik98@gmail.com>
2023-09-23 14:39:43 +0300

    create package String for learning

---
+++ src/main/java/org/Epam/Strings/String_1.java
@@ -0,0 +1,11 @@
+package org.Epam.Strings;
+
+public class String_1 {
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        String s2 = new String(s1);
+
+        System.out.println(s1 ==s2);
+    }
+}
