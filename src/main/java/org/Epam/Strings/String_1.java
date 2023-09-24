38f9985bb852c9935db287c6109c6710088cdf79
Nazar-Mik <nazarmik98@gmail.com>
2023-09-24 12:21:18 +0300

    equals 2 strings

--- src/main/java/org/Epam/Strings/String_1.java
+++ src/main/java/org/Epam/Strings/String_1.java
@@ -4,12 +4,11 @@ public class String_1 {
     public static void main(String[] args) {
 
         String s1 = "abc";
-        String s2 = new String(s1);
+        String s2 = new String("abcdddddddd");
 
-        System.out.println(s1 ==s2);
-
-        s1 = s1 + "123";
-        s1 = s1 + null;
-        System.out.println("s1 = "  + s1);
+        System.out.println(s1 == s2.intern());
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
     }
+
 }
