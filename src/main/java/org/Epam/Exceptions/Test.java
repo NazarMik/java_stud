31dc2d30c1ee1968b609adf571ba7e6cf945c650
Nazar-Mik <nazarmik98@gmail.com>
2023-09-24 12:55:47 +0300

    create commit from IDEA

--- src/main/java/org/Epam/Exceptions/Test.java
+++ src/main/java/org/Epam/Exceptions/Test.java
@@ -2,6 +2,14 @@ package org.Epam.Exceptions;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        throw new Exception();
+        boolean flag = true;
+        try{
+            if (flag) {
+                throw new Exception("some Error");
+            }
+            System.out.println("Other work");
+        }catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
