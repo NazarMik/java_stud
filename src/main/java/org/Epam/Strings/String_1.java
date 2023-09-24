a4558903326a845f2630427bd60ba44ca4c38e13
Nazar-Mik <nazarmik98@gmail.com>
2023-09-24 12:28:16 +0300

    add new code

--- src/main/java/org/Epam/Strings/String_1.java
+++ src/main/java/org/Epam/Strings/String_1.java
@@ -9,6 +9,13 @@ public class String_1 {
         System.out.println(s1 == s2.intern());
         System.out.println(s1.equals(s2));
         System.out.println(s1.compareTo(s2));
-    }
 
+        StringBuffer sb = new StringBuffer("abc");
+        System.out.println(sb.length() + " " + sb.capacity());
+
+        String s3 = "123";
+        String s4 = s3;
+        System.out.println(System.identityHashCode(s4));
+        System.out.println(System.identityHashCode(s3));
+    }
 }
