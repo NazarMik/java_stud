bf4aa2555ddde67e8963fbc3f75c9bfa9d5ac529
Nazar Mik <nazarmik98@gmail.com>
2023-10-04 14:24:26 +0300

    create ternary class

---
+++ src/main/java/org/study_books/chapter_4/Ternary.java
@@ -0,0 +1,16 @@
+package org.study_books.chapter_4;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int i, k;
+        i = 10;
+        k = i<0 ? -i : i;
+        System.out.println(i + " == " + k);
+        i=-10;
+        k = i<0 ? -i : i;
+        System.out.println(i + " == " + k);
+
+
+    }
+}
