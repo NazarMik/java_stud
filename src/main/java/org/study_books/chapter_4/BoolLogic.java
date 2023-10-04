f815b09af11fb117f557d298a815fd1e2ae934c1
Nazar Mik <nazarmik98@gmail.com>
2023-10-04 14:21:02 +0300

    create some code

--- src/main/java/org/study_books/chapter_4/BoolLogic.java
+++ src/main/java/org/study_books/chapter_4/BoolLogic.java
@@ -2,6 +2,20 @@ package org.study_books.chapter_4;
 
 public class BoolLogic {
     public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = a | b;
+        boolean d = a & b;
+        boolean e = a ^ b;
+        boolean f = (!a & b) | (a & !b);
+        boolean g = !a;
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
     }
 }
