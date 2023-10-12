5be0c905e7d92c9a239949a13e9309218edd9a9a
Nazar Mik <nazarmik98@gmail.com>
2023-10-12 12:42:03 +0300

    create class boxDemo in chapter_6

--- src/main/java/org/study_books/chapter_6/Box.java
+++ src/main/java/org/study_books/chapter_6/Box.java
@@ -5,3 +5,16 @@ public class Box {
     double height;
     double depth;
 }
+class BoxDemo{
+    public static void main(String[] args) {
+        Box box = new Box();
+        double vol;
+
+        box.depth = 21;
+        box.height = 22;
+        box.width = 23;
+
+        vol = box.depth * box.height * box.width;
+        System.out.println(vol);
+    }
+}
