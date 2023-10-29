b51cceb3ef3625442cddde3702785897980c3fb3
Nazar-Mik <nazarmik98@gmail.com>
2023-10-29 21:16:48 +0200

    for commits

--- src/main/java/org/Epam/Strings/String_1.java
+++ src/main/java/org/Epam/Strings/String_1.java
@@ -5,7 +5,7 @@ public class String_1 {
 
         System.out.println("dcedtbgvev");
         String s1 = "abcd";
-        String s2 = new String("abcdddfddd");
+        String s2 = new String("abddd");
 
         System.out.println(s1 == s2.intern());
         System.out.println(s1.equals(s2));
