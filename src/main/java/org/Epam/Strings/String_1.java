2240254e20fc777b43f54f42555aaeb9484594df
Nazar-Mik <nazarmik98@gmail.com>
2023-11-06 11:27:25 +0200

    for commits

--- src/main/java/org/Epam/Strings/String_1.java
+++ src/main/java/org/Epam/Strings/String_1.java
@@ -5,7 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class String_1 {
     public static void main(String[] args) {
 
-        System.out.println("dvамикикпv");
+        System.out.println("dvамипv");
         String s1 = "abcd";
         String s2 = new String("abdпкd");
 
